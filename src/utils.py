6a1ce59fd9eb6234d2b5c939721f1c6a6cf21ab9
Sheelabhadra Dey <sheelabhadra@gmail.com>
2019-05-27 19:22:00 -0500

    Updated Priorityqueue

--- src/utils.py
+++ src/utils.py
@@ -1,24 +1,6 @@
 from typing import Dict, List
 import heapq
 
-# class PriorityQueue:
-#     def __init__(self):
-#         self._queue = []
-#         self._index = 0
-
-#     def insert(self, item, priority):
-#         heapq.heappush(self._queue, (priority, self._index, item))
-#         self._index += 1
-
-#     def remove(self):
-#         return heapq.heappop(self._queue)[-1]
-
-#     def is_empty(self):
-#         return len(self._queue) == 0
-
-#     def size(self):
-#         return len(self._queue)
-
 class PriorityQueue:
     def __init__(self):
         self._queue = []
